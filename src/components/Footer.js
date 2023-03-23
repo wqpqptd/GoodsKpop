9324ba14993147a0761c4239262523475e518740
shyns <quynhanh21082k1@gmail.com>
2023-03-23 21:57:04 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="py-3"></footer>
+            <footer className="py-3"></footer>
+            <footer className="py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="text-center mb-0 text-white">
+                                &copy; {new Date().getFullYear()}; Power Developer's Corner 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
